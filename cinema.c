8fd546e6818aa7d8c2511291e69b5fb6b1166665
nalualvarez <nalualvarez@hotmail.com>
2019-10-30 13:33:32 -0300

    Inicio do trabalho

---
+++ cinema.c
@@ -0,0 +1,45 @@
+// Autores: Ana Luísa Salvador Alvarez 160048036 e Gabriel Rocha Fontenele 150126760
+// Atividade: Trabalho Prático
+
+// CINEMA
+
+// F filmes
+// S salas com capacidade C de pessoas
+// Z pessoas
+
+// Cada vez que um filme acaba em uma sala, comeca um novo filme que que nao esta sendo exibido em nenhuma outra sala.
+// Cada pessoa no cinema quer ver todos os filmes em cartaz 1 vez.
+// Pessoa sempre procura por um filme que ainda nao viu.
+// Se achar e sala nao estiver cheia, entra na secao.
+// Se achar e sala estiver cheia, procura por outro filme que ainda nao viu.
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+
+
+int FILMES = 10;
+int SALAS = 5;
+int CAPACIDADE = 50;
+int PESSOAS = 1000;
+
+
+int assiste (){
+	
+}
+
+
+
+int main(){
+
+	
+	
+	return 0;
+
+}
+
+
