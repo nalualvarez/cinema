6224153f869da9fedd376e9af26f1ded0d741fa4
nalualvarez <nalualvarez@hotmail.com>
2019-11-05 11:27:17 -0300

    criação de funções e tipos

--- cinema.c
+++ cinema.c
@@ -20,22 +20,45 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-
-
 int FILMES = 10;
 int SALAS = 5;
 int CAPACIDADE = 50;
 int PESSOAS = 1000;
 
 
-int assiste (){
+typedef struct{ //tipo de variavel com nome e veotr de quais filmes a pessoa ja viu (indice do vetor recebe: 1 = viu, 0 = nao viu)
+	char nome[20];
+	int filmes[FILMES];
+} Pessoa;
+
+Pessoa todos[PESSOAS];
+
+void assiste (){
 	
+}
+
+int passafilme(){ //gera randomicamente qual filme vai ser passado
+	int vaipassar;
+	vaipassar = rand()%FILMES;
+	return vaipassar;
 }
 
 
 
 int main(){
-
+	
+	int i, j;
+	
+	//zera vetor de filmes assistidos
+	for (i=0; i<PESSOAS; i++){
+		for(j=0;j<FILMES;j++)
+		todos[i].filmes[j] = 0;
+	}
+	
+	while(1){ //roda direto pegando sala randomicamente + filme randomicamente e testa se as pessoas ja assistiram
+		for ()
+	}
+	
 	
 	
 	return 0;
